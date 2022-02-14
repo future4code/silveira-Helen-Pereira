4edd0f0d48b66ab2184f732135f0dea618b49a6c
Helen Carolini <carolinihelen@hotmail.com>
2022-02-14 20:32:48 -0300

    aula-variaveis

--- modulo1/variaveis/template_javascript/index.js
+++ modulo1/variaveis/template_javascript/index.js
@@ -8,6 +8,19 @@ Exercicios de escrita
 1- O typeof ele vai executar o comando, vai ler ele, mais não consegue imprimir.Porque quem imprime direto no console é o console.log
 
  */
+/*
+console.log(typeof nome);
+console.log(typeof idade);
+
+const nome = prompt ("Qual seu nome?");
+const idade = prompt ("Qual sua idade?");
+
+nome = "Helen";
+idade = 25
+
+console.log("Olá", nome, "você tem", idade "anos");
+*/
+
 
 /* 
 console.log("Olá mundo!");
@@ -39,4 +52,44 @@ console.log(typeof nome);
 
  console.log(nome, idade);
  */
+
+
+
+ /*
+ const usando = prompt("Você está usando uma roupa azul hoje?");
+ const olhos = prompt("Você tem olhos castanhos?");
+ const agua = prompt("Você bebeu aguá hoje?")
+
+ console.log(usando, olhos, agua);
+ */
+
+/*
+const nome = "Helen";
+const sobreNome = "Pereira";
+let idade = 25;
+let souEstudante = true;
+
+ console.log("Olá" ,nome, "você tem" , idade, "anos");
+
+ let voceEstaUsandoUmaRoupaAzulHoje = true;
+ console.log(voceEstaUsandoUmaRoupaAzulHoje);
+ */
+
+ /*
+ let a = 10
+ let b = 25
+ let c = 0
+
+ c = a
+ a = b
+ b = a
+
+ console.log(a);
+ console.log(c);
+ console.log(b);
+ */
+
+
+
+
 
