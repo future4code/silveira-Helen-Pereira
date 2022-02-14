32bcb4b8327db3054de062cc447faf1b8b2380d4
Helen Carolini <carolinihelen@hotmail.com>
2022-02-14 14:14:11 -0300

    aula-variaveis

---
+++ modulo1/variaveis/template_javascript/index.js
@@ -0,0 +1,42 @@
+/*
+1- Será impresso 10
+                 10 5
+2- Será impresso 10 10 10    
+
+
+Exercicios de escrita
+1- O typeof ele vai executar o comando, vai ler ele, mais não consegue imprimir.Porque quem imprime direto no console é o console.log
+
+ */
+
+/* 
+console.log("Olá mundo!");
+console.log("Boa tarde pessoal");
+
+const nome = prompt("Qual é o seu nome?");
+const idade = prompt("Qual é a sua idade?");
+
+
+console.log("meu nome é", nome, "e minha idade é", idade)
+*/
+
+
+
+/*
+const nome = "Helen";
+const sobreNome = "Pereira";
+let idade = 25;
+let souEstudante = true;
+
+console.log(nome, sobreNome, idade,  souEstudante);
+
+console.log(typeof nome);
+*/
+
+/*
+ const nome = prompt("Olá, qual seu nome?");
+ const idade = prompt("Qual sua idade?");
+
+ console.log(nome, idade);
+ */
+
