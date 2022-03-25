07936175bbf4a81dd93613ac69a48d1cf44d60d2
Helen Carolini <carolinihelen@hotmail.com>
2022-03-25 17:00:11 -0300

    projeto-whatslab

---
+++ modulo2/projeto-whatslab/projeto-whatslab/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+export default () => {
+  return (
+    <div className='app-whats'>
+    <div className='contentArea'>
+      ....
+    </div>
+    <footer>
+    <div className='input'>
+      <input placeholder='Usuario'></input>
+      <input placeholder='Mensagem'></input>
+      <button>Enviar</button>
+      </div>
+    </footer>
+    </div>
+  )
+}
