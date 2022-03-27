e4544f19da83034f2375a8c9d3fc179da6976031
Helen Carolini <carolinihelen@hotmail.com>
2022-03-27 19:52:16 -0300

    projeto-whatslab

--- modulo2/projeto-whatslab/projeto-whatslab/src/App.js
+++ modulo2/projeto-whatslab/projeto-whatslab/src/App.js
@@ -1,18 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
+import ChatIntro from './components/ChatIntro';
+import usuarioAtivo from './components/usuarioAtivo';
+
 export default () => {
+
+  const [usuarioAtivo, secaoIputUsuario] = useState({});
+
   return (
     <div className='app-whats'>
     <div className='contentArea'>
-      ....
+        {usuarioAtivo. chatNome !== undefined &&
+        
+        <usuarioAtivo />
+        
+        }
+      <ChatIntro />
+
     </div>
+   
     <footer>
-    <div className='input'>
-      <input placeholder='Usuario'></input>
-      <input placeholder='Mensagem'></input>
-      <button>Enviar</button>
+    <div className='inputUsuario'>
+     
+      <input placeholder='usuario' ></input>
       </div>
+      <div className='inputMensagem'>
+      <input placeholder='Mensagem'></input>
+      </div>
+      <button>Enviar</button>
+      
     </footer>
     </div>
   )
