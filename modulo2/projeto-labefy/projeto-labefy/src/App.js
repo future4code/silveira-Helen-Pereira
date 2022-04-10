57ed22763891db2bad58791a7b5221bb4d4af686
Helen Carolini <carolinihelen@hotmail.com>
2022-04-10 15:45:56 -0300

    projeto-labefy

--- modulo2/projeto-labefy/projeto-labefy/src/App.js
+++ modulo2/projeto-labefy/projeto-labefy/src/App.js
@@ -1,12 +1,53 @@
-import React from "react";
+import React from 'react';
+import Playlists from './Components/PlayList';
+import styled from 'styled-components';
+import AdicionarMusicas from './Components/AdicionarMusica';
+import Header from './Components/Header';
 
 
-function App() {
-  return (
-    <div >
-     
-    </div>
-  );
-}
 
-export default App;
+const Background = styled.div`
+  width: 100%;
+  min-height: 100%;
+  background-color: #e2eafc;
+  background-repeat: repeat-y;
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+  align-items: center
+`
+
+
+export default class App extends React.Component {
+  state = {
+    mudarTela: "mostrarPlaylists",
+  }
+
+
+  mudaTela = () => {
+     this.setState({ mudarTela: !this.state.mudarTela});
+   
+  }
+
+
+  render() {
+
+    const mudancaDeTela = () => {
+      if (this.state.mudarTela === "mostrarPlaylists") {
+        return <Playlists />
+    } else if (this.state.mudarTela === "mostrarAddMusicas") {
+        return <AdicionarMusicas />
+    }
+  }
+
+    return(
+      <Background>
+
+        <Header />
+        {mudancaDeTela()}
+
+      </Background> 
+    );
+  }
+      
+};
